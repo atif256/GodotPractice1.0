51ba3281da315f7afbaccce2b3f05801c38c8491
atif256 <atifkunn@gmail.com>
2023-12-27 23:58:39 +0800

    add new traps func

---
+++ Traps/WormTraps.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var damage: int = 50
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+func _ready():
+	animation_tree.active = true
+
+
+func _on_player_detector_body_entered(body):
+	for child in body.get_children():
+		if child is PlayerHealth:
+			child.hit(damage,Vector2.ZERO)
