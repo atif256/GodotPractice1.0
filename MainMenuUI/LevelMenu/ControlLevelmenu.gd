75f7a345feff502822d461e741d939f998a4902d
atif256 <atifkunn@gmail.com>
2023-12-31 19:48:06 +0800

    Design Main Menu UI
    several design for main menu, pause menu, level ad so on

--- MainMenuUI/LevelMenu/ControlLevelmenu.gd
+++ MainMenuUI/LevelMenu/ControlLevelmenu.gd
@@ -6,12 +6,10 @@ extends Control
 
 
 #tutorial level
-func _on_tutorial_pressed():
-	get_tree().change_scene_to_packed(tutorial_level)
+
 
 #level one scene
-func _on_level_1_pressed():
-	get_tree().change_scene_to_packed(level_one)
+
 
 
 #back to menu button
@@ -19,5 +17,19 @@ func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://MainMenuUI/MainMenu/main_menu.tscn")
 
 
+
+
+#new design
+func _on_tutorial_pressed():
+	get_tree().change_scene_to_packed(tutorial_level)
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_packed(level_one)
+
 func _on_level_2_pressed():
 	get_tree().change_scene_to_packed(level_two)
+
+
+#new back button design
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://MainMenuUI/MainMenu/main_menu.tscn")
--- MainMenuUI/MainMenu/ControlMenu.gd
+++ MainMenuUI/MainMenu/ControlMenu.gd
@@ -4,10 +4,23 @@ extends Control
 func _on_levels_pressed():
 	get_tree().change_scene_to_file("res://MainMenuUI/LevelMenu/levels_menu.tscn")
 
-#to quit the game
-func _on_quit_pressed():
-	get_tree().quit()
+
 
 #to options scene
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://MainMenuUI/OptionMenu/options_menu.tscn")
+
+
+
+#new design button
+#to level scene
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://MainMenuUI/LevelMenu/levels_menu.tscn")
+
+#to options scene
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://MainMenuUI/OptionMenu/options_menu.tscn")
+
+#to quit the game
+func _on_quit_pressed():
+	get_tree().quit()
