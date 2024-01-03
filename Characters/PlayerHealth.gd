5140a8eddb13d64cd1ead98a28115b617c5daf24
atif256 <atifkunn@gmail.com>
2024-01-03 14:13:10 +0800

    updating health bar ui

--- Characters/PlayerHealth.gd
+++ Characters/PlayerHealth.gd
@@ -11,20 +11,38 @@ signal on_heal(node: Node, heal_taken: int)
 	get:
 		return health
 	set(value):
-		health = value
+		#health = value
+		health = clamp(value, 0, max_health)
+		update_health_bar()
 
+@export var max_health: float = 100
 @export var dead_animation_name: String = "dead"
+var health_bar: TextureProgressBar = null
+
+func _ready():
+	health_bar = $"../PHealthBar" #here is the reference problem
+	# new added
+	update_health_bar()
+
 
 func hit(damage: int, knockback_direction: Vector2):
 	health -= damage
-	#print_debug(health)
 	emit_signal("on_hit", get_parent(), damage, knockback_direction)
+	update_health_bar()
 
 #to pick up the heal item
 func heal(heal: int):
 	health += heal
 	emit_signal("on_heal", get_parent(), heal)
+	if health > 100:
+		health = 100
+	
+	update_health_bar()
 
 func _on_animation_tree_animation_finished(anim_name):
 	if (anim_name == dead_animation_name):
 		get_parent().queue_free()
+
+func update_health_bar():
+	if health_bar:
+		health_bar.value = health
--- Characters/player.gd
+++ Characters/player.gd
@@ -46,3 +46,5 @@ func update_facing_direction():
 		sprite.flip_h = true
 	
 	emit_signal("facing_direction_changed", !sprite.flip_h)
+
+
